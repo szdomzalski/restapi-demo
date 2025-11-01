6d9dd92f21b3a01672d799ea2c8ff10b5da77b08
Szymon Domżalski <100307341+szdomzalski@users.noreply.github.com>
2025-11-01 22:30:18 +0100

    Added RUD operations on single object

--- people.py
+++ people.py
@@ -7,18 +7,18 @@ def get_timestamp() -> str:
     return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
 
-MOCKED_PEOPLE_DATA = {
-    "Fairy": {
+MOCK_PEOPLE = {
+    "fairy": {
         "fname": "Tooth",
         "lname": "Fairy",
         "timestamp": get_timestamp(),
     },
-    "Ruprecht": {
+    "ruprecht": {
         "fname": "Knecht",
         "lname": "Ruprecht",
         "timestamp": get_timestamp(),
     },
-    "Bunny": {
+    "bunny": {
         "fname": "Easter",
         "lname": "Bunny",
         "timestamp": get_timestamp(),
@@ -27,19 +27,51 @@ MOCKED_PEOPLE_DATA = {
 
 
 def read_all() -> list[dict[str, str]]:
-    return [*MOCKED_PEOPLE_DATA.values()]
+    return [*MOCK_PEOPLE.values()]
 
 
 def create(person: dict[str, str]) -> tuple[dict[str, str], int]:
     lname = person['lname']  # Required field
     fname = person.get("fname", "")
 
-    if lname and lname not in MOCKED_PEOPLE_DATA:
-        MOCKED_PEOPLE_DATA[lname] = {
-            "lname": lname,
-            "fname": fname,
-            "timestamp": get_timestamp(),
-        }
-        return MOCKED_PEOPLE_DATA[lname], HTTPStatus.CREATED
-    else:
-        abort(HTTPStatus.NOT_ACCEPTABLE, f'Person with last name {lname} already exists')
+    if not lname:
+        abort(HTTPStatus.BAD_REQUEST, "Last name is required")
+
+    last_name_insensitive = lname.casefold()
+
+    if last_name_insensitive in MOCK_PEOPLE:
+        abort(HTTPStatus.CONFLICT, f"Person with last name '{lname}' already exists")
+
+    MOCK_PEOPLE[last_name_insensitive] = {
+        "lname": lname,
+        "fname": fname,
+        "timestamp": get_timestamp(),
+    }
+    return MOCK_PEOPLE[last_name_insensitive], HTTPStatus.CREATED
+
+
+def read_one(lname: str) -> dict[str, str]:
+    lname_insensitive = lname.casefold()
+    try:
+        return MOCK_PEOPLE[lname_insensitive]
+    except KeyError:
+        abort(HTTPStatus.NOT_FOUND, f'Person with last name {lname} not found')
+
+
+def update(lname: str, person: dict[str, str]) -> dict[str, str]:
+    lname_insensitive = lname.casefold()
+    try:
+        MOCK_PEOPLE[lname_insensitive]['fname'] = person.get('fname', MOCK_PEOPLE[lname_insensitive]['fname'])
+        MOCK_PEOPLE[lname]["timestamp"] = get_timestamp()
+        return MOCK_PEOPLE[lname_insensitive]
+    except KeyError:
+        abort(HTTPStatus.NOT_FOUND, f'Person with last name {lname} not found')
+
+
+def delete(lname: str) -> tuple[str, int]:
+    lname_insensitive = lname.casefold()
+    try:
+        del MOCK_PEOPLE[lname_insensitive]
+        return None, HTTPStatus.NO_CONTENT
+    except KeyError:
+        abort(HTTPStatus.NOT_FOUND, f'Person with last name {lname} not found')
